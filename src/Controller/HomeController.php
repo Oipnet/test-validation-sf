5bb193cfe078b22a4c40c6c65d12ea9e3bb482f9
Arnaud POINTET <arnaud.pointet@gmail.com>
2018-05-08 16:59:04 +0200

    Add form in controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Twig\Environment;
 
-class HomeController extends Controller
+class HomeController
 {
+    /**
+     * @var FormFactoryInterface
+     */
+    private $form;
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(FormFactoryInterface $form, Environment $twig)
+    {
+        $this->form = $form;
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $post = new Post();
+
+        $form = $this->form->createBuilder(PostType::class, $post)->getForm();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form);die();
+        }
+
+        return new Response($this->twig->render('home/index.html.twig', [ 'form' => $form->createView() ]));
     }
 }
